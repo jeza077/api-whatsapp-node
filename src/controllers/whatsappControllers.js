5a1f5e318843295186aba8522b5636612313e2c2
JEZA <jesus.zuniga077@gmail.com>
2022-12-19 21:22:27 -0600

    actualizacion

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,15 +1,17 @@
 const VerifyToken = (req, res) => {
 
     try {
-        var accessToken = "ERGFWEIMWE84654AED8WEF658EEW14";
-        var token = req.query["hub.verify.token"];
-        var challenge = req.query["hub.challenge"];
+        // var accessToken = "ERGFWEIMWE84654AED8WEF658EEW14";
+        // var token = req.query["hub.verify.token"];
+        // var challenge = req.query["hub.challenge"];
 
-        if(challenge != null && token != null && token == accessToken) {
-            res.send(challenge);
-        } else {
-            res.status(400).send();
-        }
+        // if(challenge != null && token != null && token == accessToken) {
+        //     res.send(challenge);
+        //     res.send('Holaaaaa')
+        // } else {
+        //     res.status(400).send();
+        // }
+         res.send("Hola verifyToken");
 
 
     } catch (e) {
