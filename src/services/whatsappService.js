37c958103cba78d41a8c56137f1c3d81d0b0e292
JEZA <jesus.zuniga077@gmail.com>
2022-12-27 23:03:08 -0600

    Token nuevo

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -23,7 +23,7 @@ const SendMessageWhatsapp = (textResponse, number) => {
         body: data,
         headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer EAAubQGSCTC8BAIH6hf3zlfy9tCjPlLOZAC0rzsby3upm450Ek9ZA0iEYNBLwJAOo1NJNNzFNMzTYUXH9UVTOjuTseMkCJATptzUmTUHiLpqv7zjHqr3PrtPVUNIhyjroREI5mVDZA6TcnmV5xp39UPd5Hi9fwfHrrO0BZBMvT9I8wtviF2lA"
+            Authorization: "Bearer " + process.env.TOKEN_WA
         }
     };
 
