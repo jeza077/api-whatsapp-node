ab69ea3a951971a591e4001e62b5a251783e3f10
JEZA <jesus.zuniga077@gmail.com>
2023-02-22 22:43:04 -0600

    Prueba9

--- src/shared/processMessage.js
+++ src/shared/processMessage.js
@@ -14,8 +14,8 @@ const res = require('express/lib/response');
 const Process = (textUser, number) => {
 
     console.log('desde process', textUser);
-    console.log('desde process type', textUser['type']);
-    console.log('desde process type objet', textUser.type);
+    console.log('desde process textbody', (textUser['text'])['body']);
+    console.log('desde process typeObjet', textUser.text.body);
     /*
     let textUserLower;
     if (typeof textUser == "string"){
