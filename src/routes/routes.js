37879910960e5f97948096c017c08315c322b7bc
JEZA <jesus.zuniga077@gmail.com>
2023-03-08 20:59:36 -0600

    Probando

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const whatsappController = require('../controllers/whatsappControllers');
 
+const app = express();
+
 router
 .get("/", whatsappController.VerifyToken)
 .post("/", whatsappController.ReceiveMessage)
 
+app.use(express.static(__dirname + '/public'))
+console.log(app.use(express.static(__dirname + '/public')))
 
 module.exports = router;
