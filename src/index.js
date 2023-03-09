e1b32bd320138d9f58dcefc290cddae7d5125aaf
JEZA <jesus.zuniga077@gmail.com>
2023-03-08 20:42:16 -0600

    Probando App para enviar a WA orden

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(express.static(__dirname + '/public'))
+
 app.use("/whatsapp", apiRoute);
 
 app.listen(PORT, () => {console.log("El puerto es: " + PORT)});
